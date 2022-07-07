8ba60cd67ba3afa76d4f06d86e93b6693e933d3e
naturalmess <brytkhalifa@gmail.com>
2022-07-07 23:53:59 +0200

    added anotation package

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
